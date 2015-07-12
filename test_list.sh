c769bb664c368a2d2a6140a6c5261e4de5a5851a
Hai Nguyen <hainm@users.noreply.github.com>
2015-07-12 06:09:18 -0400

    Update test_list.sh

--- test_list.sh
+++ test_list.sh
@@ -6,3 +6,8 @@ python test_autoimage.py || exit 1
 python test_autoimage_with_rmsfit.py || exit 1
 python test_chunk_iter_with_autoimage_rmsfit.py || exit 1
 python test_frame_iter_with_autoimage.py || exit 1
+
+python test_ActionList_distance.py || exit 1
+python test_ActionList_angle.py || exit 1
+
+python test_ActionList_dihedral.py  || exit 1
