580ad43255cda9ed4e8ba8a24a62ed03ff35cf83
Hai Nguyen <hainm.comp@gmail.com>
2015-07-12 03:56:48 -0400

    pt tset

---
+++ setup_env.sh
@@ -0,0 +1,21 @@
+# copied from `ParmEd`
+MINICONDA=Miniconda-latest-Linux-x86_64.sh
+MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget http://repo.continuum.io/miniconda/$MINICONDA
+if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+    echo "Miniconda MD5 mismatch"
+    exit 1
+fi
+bash $MINICONDA -b
+
+export PATH=$HOME/miniconda/bin:$PATH
+conda install --yes conda-build jinja2 binstar pip
+conda create -y -n myenv python=$PYTHON_VERSION numpy h5py
+
+source activate myenv
+
+# install other packages here
+conda install -c https://conda.binstar.org/omnia mdtraj
+pip install git+git://github.com/swails/ParmEd
+pip install memory_profiler
+pip install psutil
