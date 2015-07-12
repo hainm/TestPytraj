8f5d25d1b9fb18d91f6ff9cfea5e4c4f53ea150e
Hai Nguyen <hainm@users.noreply.github.com>
2015-07-12 06:26:25 -0400

    Update test_list.sh

--- test_list.sh
+++ test_list.sh
@@ -2,14 +2,15 @@ python test_matrix_analysis.py || exit 1
 python test_dihedral_analysis.py || exit 1
 python test_Trajectory_constructor.py || exit 1
 
+echo "test autoimage, frame_iter, chunk_iter, mask"
 python test_autoimage.py || exit 1
 python test_autoimage_with_rmsfit.py || exit 1
 python test_chunk_iter_with_autoimage_rmsfit.py || exit 1
 python test_frame_iter_with_autoimage.py || exit 1
 
+echo "test basic geom"
 python test_ActionList_distance.py || exit 1
 python test_ActionList_angle.py || exit 1
-
 python test_ActionList_dihedral.py  || exit 1
 
 echo "test_load_mdtraj_0.py"; python test_load_mdtraj_0.py || exit 1
