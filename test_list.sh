b01ae1d6e0a96e3a0525aa1bda80dad67d9046db
Hai Nguyen <hainm@users.noreply.github.com>
2015-07-12 06:14:56 -0400

    Update test_list.sh

--- test_list.sh
+++ test_list.sh
@@ -11,3 +11,5 @@ python test_ActionList_distance.py || exit 1
 python test_ActionList_angle.py || exit 1
 
 python test_ActionList_dihedral.py  || exit 1
+
+python test_memory_accumulation.py || exit 1
