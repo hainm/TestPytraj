c2a322c2298787373ab82df7f7c90b034dd07739
Hai Nguyen <hainm.comp@gmail.com>
2015-10-15 14:50:00 -0400

    update test

--- setup_env.sh
+++ setup_env.sh
@@ -1,21 +1,18 @@
-# copied from `ParmEd`
-MINICONDA=Miniconda-latest-Linux-x86_64.sh
-MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
-    echo "Miniconda MD5 mismatch"
-    exit 1
-fi
-bash $MINICONDA -b
+#!/bin/sh
+
+# install miniconda
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 binstar pip
-conda create -y -n myenv python=$PYTHON_VERSION numpy h5py cython
+# do now install cython here. Do it later with different version on .travis.yaml
+conda create -y -n myenv python=$PYTHON_VERSION numpy h5py
 
 source activate myenv
 
 # install other packages here
-pip install git+git://github.com/swails/ParmEd
+pip install git+git://github.com/ParmEd/ParmEd
 pip install memory_profiler
 pip install psutil
 pip install cclib
