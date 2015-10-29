2b6ab4f04053c613304d0c033daf65bd79718177
Hai Nguyen <hainm@users.noreply.github.com>
2015-10-28 20:20:45 -0400

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -7,7 +7,7 @@ bash miniconda.sh -b
 export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 binstar pip
 # do now install cython here. Do it later with different version on .travis.yaml
-conda create -y -n myenv python=$PYTHON_VERSION numpy h5py
+conda create -y -n myenv python=$PYTHON_VERSION numpy h5py mpi4py
 
 source activate myenv
 
