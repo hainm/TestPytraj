2f351490a963c691db8c90ca7d0cdeb23461ba41
Hai Nguyen <hainm@users.noreply.github.com>
2016-03-08 01:30:30 -0500

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # install miniconda
-wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh;
+    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+        wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-MacOSX-x86_64.sh -O miniconda.sh;
+    else
+        wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh;
+    fi
 bash miniconda.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 binstar pip
 # do now install cython here. Do it later with different version on .travis.yaml
-conda create -y -n myenv python=$PYTHON_VERSION numpy h5py mpi4py
+conda create -y -n myenv python=$PYTHON_VERSION numpy
 
 source activate myenv
-
-# install other packages here
-pip install git+git://github.com/ParmEd/ParmEd
-pip install memory_profiler
-pip install psutil
-pip install cclib
