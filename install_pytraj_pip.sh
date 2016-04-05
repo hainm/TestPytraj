279230dec81c80cf0a571a40efa8c62b81df6888
Hai Nguyen <hainm.comp@gmail.com>
2016-04-05 02:44:36 -0400

    oops, try adding cpptraj

--- install_pytraj_pip.sh
+++ install_pytraj_pip.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/hainm/pytraj
+git clone https://github.com/amber-md/pytraj
 cd pytraj
+
+git clone https://github.com/amber-md/cpptraj
 
 pip install pytraj
 python run_tests.py
