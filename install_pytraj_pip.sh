0817c401787616adbb9c33fd9c45cb4ee8589bcb
Hai Nguyen <hainm.comp@gmail.com>
2016-04-05 16:33:08 -0400

    try osx anaconda

--- install_pytraj_pip.sh
+++ install_pytraj_pip.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+
+# just clone to get test files
 git clone https://github.com/amber-md/pytraj
 cd pytraj
 
+# just clone to get saved test files
 git clone https://github.com/amber-md/cpptraj
 
-pip install pytraj
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    pip install -i https://pypi.anaconda.org/ambermd/simple pytraj
+else
+    pip install pytraj
+fi
+
 python run_tests.py
