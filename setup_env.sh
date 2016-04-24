832e71c418eb0d72d06f40560f1dd2c830cb846c
Hai Nguyen <hainm.comp@gmail.com>
2016-04-23 23:06:51 -0400

    add pyflakes

--- setup_env.sh
+++ setup_env.sh
@@ -11,7 +11,7 @@ bash miniconda.sh -b
 export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 binstar pip
 # do now install cython here. Do it later with different version on .travis.yaml
-conda create -y -n myenv python=$PYTHON_VERSION numpy nose mpi4py libnetcdf openblas
+conda create -y -n myenv python=$PYTHON_VERSION numpy nose mpi4py libnetcdf openblas pyflakes
 
 source activate myenv
 pip install https://github.com/ParmEd/ParmEd/archive/master.zip
