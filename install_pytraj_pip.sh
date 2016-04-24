8d66a914fbf4a85e37e368873d9206591a800cf8
Hai Nguyen <hainm.comp@gmail.com>
2016-04-24 13:41:46 -0400

    try

--- install_pytraj_pip.sh
+++ install_pytraj_pip.sh
@@ -5,6 +5,9 @@
 git clone https://github.com/amber-md/pytraj
 cd pytraj
 
+# check recent version that does not have 'esander'
+git checkout 87639ddfc893a30c07c695a9913553e1c809eda7
+
 # just clone to get saved test files
 git clone https://github.com/amber-md/cpptraj
 
