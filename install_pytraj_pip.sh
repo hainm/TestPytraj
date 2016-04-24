74f63d894ac8e706740cdb36760619261d511e23
Hai Nguyen <hainm.comp@gmail.com>
2016-04-23 22:03:24 -0400

    pysander, osx, py2.7 --> 3.5

--- install_pytraj_pip.sh
+++ install_pytraj_pip.sh
@@ -8,14 +8,9 @@ cd pytraj
 # just clone to get saved test files
 git clone https://github.com/amber-md/cpptraj
 
-# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#     # pip install -i https://pypi.anaconda.org/ambermd/simple pytraj
-#     pip install pytraj
-# else
-#     pip install pytraj
-# fi
-
 pip install pytraj
 
 python run_tests.py sim || exit 1
-python run_tests.py
+cd tests && nosetests energies/*py -vs
+
+# python run_tests.py
