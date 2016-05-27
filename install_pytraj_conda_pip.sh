6497adbcaa3c3cc3cdf8fada6e2e838295e22480
Hai Nguyen <hainm.comp@gmail.com>
2016-05-26 23:47:28 -0400

    fix

--- install_pytraj_conda_pip.sh
+++ install_pytraj_conda_pip.sh
@@ -5,5 +5,5 @@ git clone https://github.com/hainm/pytraj
 cd pytraj
 
 pip install -i https://pypi.anaconda.org/hainm/simple pytraj
-python run_tests.py sim || exit 1
-python run_tests.py
+cd tests
+nosetests -vs .
