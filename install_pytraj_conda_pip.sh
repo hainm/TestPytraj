e5700c8a59af7894826483342213bdf2fdaff424
Hai Nguyen <hainm.comp@gmail.com>
2016-05-27 00:23:40 -0400

    test

--- install_pytraj_conda_pip.sh
+++ install_pytraj_conda_pip.sh
@@ -4,6 +4,14 @@
 git clone https://github.com/hainm/pytraj
 cd pytraj
 
+# just clone to get saved test files
+git clone https://github.com/amber-md/cpptraj
+
 pip install -i https://pypi.anaconda.org/hainm/simple pytraj
-cd tests
+
+cd tests/fake_amberhome/
+export AMBERHOME=`pwd`
+
+cd ..
+
 nosetests -vs .
