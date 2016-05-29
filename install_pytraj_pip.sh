62a9c1bd61d1b8d4afe22f35477f54b33ef368f9
Hai Nguyen <hainm.comp@gmail.com>
2016-05-29 10:29:47 -0400

    update

--- install_pytraj_pip.sh
+++ install_pytraj_pip.sh
@@ -5,9 +5,6 @@
 git clone https://github.com/amber-md/pytraj
 cd pytraj
 
-# check recent version that does not have 'esander'
-git checkout 87639ddfc893a30c07c695a9913553e1c809eda7
-
 # just clone to get saved test files
 git clone https://github.com/amber-md/cpptraj
 
@@ -18,4 +15,4 @@ python run_tests.py sim || exit 1
 cd tests/fake_amberhome/
 export AMBERHOME=`pwd`
 
-cd .. && nosetests energies/*py -vs
+cd .. && nosetests -vs .
