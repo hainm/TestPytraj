f5db5126c57915939b66103420e3f909e243a68e
Hai Nguyen <hainm.comp@gmail.com>
2016-06-08 20:09:28 -0400

    no de

--- install_pytraj_conda.sh
+++ install_pytraj_conda.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/hainm/pytraj
 cd pytraj
 
-conda install pytraj-dev -c ambermd --yes
+conda install pytraj -c ambermd --yes
 python run_tests.py sim || exit 1
 python run_tests.py
--- install_pytraj_conda_make_docs.sh
+++ install_pytraj_conda_make_docs.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/hainm/pytraj
 cd pytraj
-conda install pytraj-dev -c ambermd --yes
+conda install pytraj -c ambermd --yes
 
 git checkout gh-pages
 make
